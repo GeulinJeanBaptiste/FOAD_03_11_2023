ff5d6478f6a3c587feb1ddc2413a0a97c7a8e75a
JB <jeanbaptiste.geulin@gmail.com>
2023-11-04 00:58:47 +0100

    boutton next

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,4 +1,7 @@
 import { catalogue } from "./catalogue.js";
+const nextbutton = document.querySelector("#next");
+/* const prevbutton = document.querySelector("#prev"); */
+
 
 let currentSlide = 0;
 
@@ -11,3 +14,28 @@ function nextSlide() {
 };
 nextSlide();
 setInterval(nextSlide, 4000);
+
+
+
+
+
+const nextEvents = (nextSlide);
+nextbutton.addEventListener("click", nextEvents);
+
+
+
+
+
+/* function prevSlide() {
+  const slide = catalogue[currentSlide];
+  document.getElementById('image').src = slide.image;
+  document.getElementById('title').innerText = slide.title;
+  document.getElementById('artiste').innerText = slide.artiste;
+  currentSlide = (currentSlide - 1) % catalogue.length;
+};
+prevSlide();
+
+
+const prevEvents = (prevSlide);
+
+prevbutton.addEventListener("click", prevEvents); */
