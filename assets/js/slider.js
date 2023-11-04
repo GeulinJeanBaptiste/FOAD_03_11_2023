2161ed8b1bd2b94f2f94521ae5d3f6d7a9c3594e
JB <jeanbaptiste.geulin@gmail.com>
2023-11-04 19:04:36 +0100

    boutton prev et next fonctionne

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,41 +1,38 @@
 import { catalogue } from "./catalogue.js";
 const nextbutton = document.querySelector("#next");
-/* const prevbutton = document.querySelector("#prev"); */
+const prevbutton = document.querySelector("#prev");
+
+globalThis.currentSlide = 0;
 
 
-let currentSlide = 0;
-
+/* Fonction pour le boutton next */
 function nextSlide() {
   const slide = catalogue[currentSlide];
+  currentSlide = (currentSlide + 1) % catalogue.length;
   document.getElementById('image').src = slide.image;
   document.getElementById('title').innerText = slide.title;
   document.getElementById('artiste').innerText = slide.artiste;
-  currentSlide = (currentSlide + 1) % catalogue.length;
 };
-nextSlide();
-setInterval(nextSlide, 4000);
-
-
-
-
 
 const nextEvents = (nextSlide);
 nextbutton.addEventListener("click", nextEvents);
+nextSlide();
 
 
+/* Fonction pour le boutton prev. */
 
-
-
-/* function prevSlide() {
+function prevSlide() {
   const slide = catalogue[currentSlide];
+  currentSlide = (currentSlide - 1 + catalogue.length) % catalogue.length;
   document.getElementById('image').src = slide.image;
   document.getElementById('title').innerText = slide.title;
   document.getElementById('artiste').innerText = slide.artiste;
-  currentSlide = (currentSlide - 1) % catalogue.length;
 };
-prevSlide();
-
 
 const prevEvents = (prevSlide);
+prevbutton.addEventListener("click", prevEvents);
 
-prevbutton.addEventListener("click", prevEvents); */
+// Pourquoi en activant prevSlide() il m'affiche d'abord la 2e image du tableau.
+/* prevSlide(); */
+
+setInterval(nextSlide, 4000);
